c62582e493a207236605ef51b9d404a1b9a4b5e0
AgustinVaras <Agustin.Varas@outlook.com.ar>
2023-05-24 13:29:35 -0300

    Creacion de la clase Carrito y de los modelos iniciales de sus metodos

---
+++ ProyectoWeb_Progra3/Clases/Carrito.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    class Carrito
+    {
+        public Carrito(List<Articulo> lista) { articulos = lista; }
+
+        public Carrito() { articulos = new List<Articulo>(); }
+
+        private List <Articulo> articulos;
+        private decimal monto;
+
+        public List<Articulo> Articulos
+        {
+            get { return articulos; }
+        }
+        public decimal Monto
+        {
+            set { monto = value; }
+            get { return monto;  }
+        }
+
+        public void CalcularMonto()
+        {
+            monto = 0;
+            foreach (Articulo art in articulos)
+            {
+                monto += art.Precio;
+            }
+        }
+
+        ~Carrito()
+        {
+            articulos.Clear();
+        }
+    }
+}

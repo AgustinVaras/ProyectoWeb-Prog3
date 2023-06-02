cd820b82861698d1099bd9f35ed59a4838f2936b
denissemaiz <denissemaiz@gmail.com>
2023-06-02 11:22:24 -0300

    Agrego estilo Cards al Catálogo de productos

--- ProyectoWeb_Progra3/WebCarrito/Catalogo.aspx.cs
+++ ProyectoWeb_Progra3/WebCarrito/Catalogo.aspx.cs
@@ -5,16 +5,17 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Conexiones;
+using Clases;
 
 namespace WebCarrito
 {
     public partial class Catalogo : System.Web.UI.Page
     {
+        public List<Articulo> ListaArticulo { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
             DatosDeArticulos articulo = new DatosDeArticulos();
-            dgvCatalogo.DataSource = articulo.listar();
-            dgvCatalogo.DataBind();
+            ListaArticulo = articulo.listar();
         }
     }
 }

074ee91d1f139726a9800ea9ebd78a6ccd229c98
Alan <alaanoriel@gmail.com>
2023-06-04 19:51:52 -0300

    Add pagina finalizar, pruebas en sitemaster

--- ProyectoWeb_Progra3/WebCarrito/FinalizarCompra.aspx.cs
+++ ProyectoWeb_Progra3/WebCarrito/FinalizarCompra.aspx.cs
@@ -4,18 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using Clases;
 
 namespace WebCarrito
 {
-    public partial class Site1 : System.Web.UI.MasterPage
+    public partial class FinalizarCompra : System.Web.UI.Page
     {
-        
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+
         }
-
-
     }
 }

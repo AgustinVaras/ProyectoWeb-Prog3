398477138864397aaf4c3daaa5db910f715c52b7
Alan <alaanoriel@gmail.com>
2023-06-04 21:00:55 -0300

    Cambios en carrito, se agrego logica para que muestre la imagen

--- ProyectoWeb_Progra3/WebCarrito/Site1.Master.cs
+++ ProyectoWeb_Progra3/WebCarrito/Site1.Master.cs
@@ -5,18 +5,23 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Clases;
+using Conexiones;
 
 namespace WebCarrito
 {
     public partial class Site1 : System.Web.UI.MasterPage
     {
-        
+        public Carrito Carro { get; private set; }
+        public List<Imagen> ListaImagen { get; set; }
+
         protected void Page_Load(object sender, EventArgs e)
         {
             Carro = new Carrito();
             if (Session["articulosAgregados"] != null)
             {
                 Carro.Articulos = (List<Articulo>)Session["articulosAgregados"];
+                ImagenesDatos imagen = new ImagenesDatos();
+                ListaImagen = imagen.Listar();
             }
         }
     }

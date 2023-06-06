adae46448c27ac8f146ace33caf01d52dbee0f24
Alan <alaanoriel@gmail.com>
2023-06-06 19:17:24 -0300

    Fix finalizar compra

--- ProyectoWeb_Progra3/WebCarrito/FinalizarCompra.aspx.cs
+++ ProyectoWeb_Progra3/WebCarrito/FinalizarCompra.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Clases;
+using Conexiones;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +11,41 @@ namespace WebCarrito
 {
     public partial class FinalizarCompra : System.Web.UI.Page
     {
+
+        public List<Articulo> ListaArticulo { get; set; }
+
+        public List<Imagen> ListaImagen { get; set; }
+        public List<Articulo> ListaCarrito = new List<Articulo>();
+
         protected void Page_Load(object sender, EventArgs e)
         {
+
+            DatosDeArticulos articulo = new DatosDeArticulos();
+
+            List<Articulo> Lista = (List<Articulo>)Session["articulosAgregados"];
+            repArticulos.DataSource = Lista;
+            repArticulos.DataBind();
+
+        }
+
+        protected void repImagenes_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item ||
+                e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                Articulo articulo = (Articulo)e.Item.DataItem;
+            
+                ImagenesDatos imagen = new ImagenesDatos();
+                ListaImagen = imagen.Buscar("IdArticulo", articulo.Id.ToString());
+            
+
+                List<Imagen> ListaSinRepetidos = imagen.removeDuplicadosImagen(ListaImagen);
+                Repeater repImagenes = (Repeater)e.Item.FindControl("repImagenes");
+            
+                repImagenes.DataSource = ListaSinRepetidos;
+                repImagenes.DataBind();
+                
+            }
 
         }
     }

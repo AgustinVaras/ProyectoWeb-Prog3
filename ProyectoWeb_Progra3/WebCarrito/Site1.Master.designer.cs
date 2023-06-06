c1daf42cd9a97fc0e676c998935a57ed6b5c5281
denissemaiz <denissemaiz@gmail.com>
2023-06-06 17:07:51 -0300

    Fx mostrar nro de articulos cargadosal carrito

--- ProyectoWeb_Progra3/WebCarrito/Site1.Master.designer.cs
+++ ProyectoWeb_Progra3/WebCarrito/Site1.Master.designer.cs
@@ -42,6 +42,15 @@ namespace WebCarrito
         protected global::System.Web.UI.WebControls.DropDownList ddlCategorias;
 
         /// <summary>
+        /// Control lblContador.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblContador;
+
+        /// <summary>
         /// Control repCategorias.
         /// </summary>
         /// <remarks>
